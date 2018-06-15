06f44c9bd062ec0c6a926c955af8a4e064c92580
Bruce Crevensten <bruce.crevensten@gmail.com>
2018-06-15 16:56:11 -0700

    initial reboot commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# AK CSC Drupal
+
+## Deploying updates to `nyx.snap.uaf.edu`
+
+To pull the latest code from the `master` branch of this repository and recompile the Sass stylesheets, do this on your local machine:
+
+`ssh -t username@nyx.snap.uaf.edu update_drupal.sh`
+
+## Local development instance with Docker
+
+### Setup Docker containers
+
+1. Install [Docker](https://www.docker.com/) if you have not already.
+
+1. Grab AK CSC files and database dump from https://csc.alaska.edu/sites/all/gimme.php, save them to your ~/Downloads folder.  These directions assume these files are named `csc.sql` and `files-csc.bz2` respectively.
+
+1. Clone this repository and extract the Drupal files into their proper location:
+
+   ```bash
+   git clone https://github.com/ua-snap/csc-www.git
+   cd csc-www
+
+   # Put the database into a folder where it will be ingested.
+   mv ~/Downloads/csc.sql database/
+
+   # Add site files and proper settings for local dev.
+
+   tar -jxvf ~/Downloads/files-csc.bz2 --strip-components=1 -C files/
+
+   # Launch containers and disable IP restrictions
+   # NOTE -- sometimes the name of the Docker image (csc-www_drupal_1) may be different.
+   # Use `docker ps` to find the right name of the image..
+   docker-compose up &
+   docker exec -i csc-www_drupal_1 php /usr/bin/drush.phar -y dis restrict_by_ip securepages
+   ```
+
+### Stop Docker containers
+
+The Docker containers can be stopped at any time with the following command:
+
+```bash
+docker-compose stop
+```
+
+This is useful if you need to start the Docker containers for a different website.
+
+### Start Docker containers
+
+Stopped Docker containers can be started with the following command:
+
+```bash
+docker-compose up
+```
+
+### Remove Docker containers
+
+If something goes wrong with your Docker containers and you would like to go through the setup instructions again, you will first need to remove your existing Docker containers for the AK CSC website with the following commands.
+
+ ```bash
+ docker-compose rm
+ ```
---
+++ themes/titan/node--event.tpl.php
@@ -0,0 +1,160 @@
+<?php switch($view_mode) { ?>
+<?php case 'teaser': /****************************** teaser */ ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix<?php if ($display_submitted): ?> indent<?php endif; ?>" <?php print $attributes; ?>>
+
+  <div class="node-header">
+
+    <?php if ($display_submitted): ?>
+      <div class="date">
+
+<?php
+
+  // Some AK CSC changes here, to use the begin date for workshops instead of
+  // the node creation date, which we use for other content types.
+
+  if(!empty($node->field_date[$node->language][0]['value'])) {
+    $raw_date = $node->field_date[$node->language][0]['value'];
+    $date = explode(' ', $raw_date);
+    $date_fields = explode('-', $date[0]);
+    $time = mktime(0, 0, 0, $date_fields[1], $date_fields[2], $date_fields[0]);
+  } else {
+    $time = $node->created;
+  }
+
+?>
+        <span class="month"><?php print format_date($time, 'custom', 'M') ?></span>
+        <span class="day"><?php print format_date($time, 'custom', 'j') ?> </span>
+        <span class="year"><?php print format_date($time, 'custom', 'Y') ?></span>
+      </div>
+    <?php endif; ?>
+
+    <div class="title-and-meta">
+      <?php print render($title_prefix); ?>
+      <h2 class="node-title" <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php
+        print render($content['field_location']);
+        print render($content['field_date']);
+      ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($display_submitted): ?>
+        <div class="meta">
+          <?php if ($display_submitted): ?>
+            <span class="submitted">
+              <?php print t('By ') . $name; ?>
+            </span>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+    </div> <!-- /.title-and-meta -->
+  </div> <!-- /.node-header -->
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php //print $user_picture; ?>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_location']);
+      hide($content['field_date']);
+
+      $block = module_invoke('node_subpages', 'block_view', 'menu_bar');
+      print render($block['content']);
+
+      print render($content);
+    ?>
+
+    <div class="links-wrapper">
+      <?php print render($content['links']); ?>
+    </div>
+  </div>
+
+  <?php print render($content['comments']); ?>
+
+</div>
+<?php break; default: /************************ all other display modes */ ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix<?php if ($display_submitted): ?> indent<?php endif; ?>" <?php print $attributes; ?>>
+
+  <div class="node-header">
+
+    <?php if ($display_submitted): ?>
+      <div class="date">
+
+<?php
+
+  // Some AK CSC changes here, to use the begin date for workshops instead of
+  // the node creation date, which we use for other content types.
+
+  if($node->type == 'event' &&
+     !empty($node->field_date[$node->language][0]['value'])) {
+    $raw_date = $node->field_date[$node->language][0]['value'];
+    $date = explode(' ', $raw_date);
+    $date_fields = explode('-', $date[0]);
+    $time = mktime(0, 0, 0, $date_fields[1], $date_fields[2], $date_fields[0]);
+  } else {
+    $time = $node->created;
+  }
+
+?>
+        <span class="month"><?php print format_date($time, 'custom', 'M') ?></span>
+        <span class="day"><?php print format_date($time, 'custom', 'j') ?> </span>
+        <span class="year"><?php print format_date($time, 'custom', 'Y') ?></span>
+      </div>
+    <?php endif; ?>
+
+    <?php
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['title_field']);
+      hide($content['field_location']);
+      hide($content['field_date']);
+    ?>
+    <div class="title-and-meta">
+      <?php print render($title_prefix); ?>
+	<?php // Some AK CSC changes here. Unlink page titles. ?>
+      <h1 class="node-title" <?php print $title_attributes; ?>><?php print_r($node->title_field['und'][0]['value']); ?></h1>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($display_submitted): ?>
+        <div class="meta">
+          <?php if ($display_submitted): ?>
+            <span class="submitted">
+              <?php print t('By ') . $name; ?>
+            </span>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+    </div> <!-- /.title-and-meta -->
+  </div> <!-- /.node-header -->
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php //print $user_picture; ?>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      print render($content['field_location']);
+      print render($content['field_date']);
+    ?>
+
+    <?php
+      $block = module_invoke('node_subpages', 'block_view', 'menu_bar');
+      if (!empty($block['content']['#markup'])):
+    ?>
+    <div id="event-sub-nav">
+      <?php print render($block['content']); ?>
+    </div>
+    <?php endif; ?>
+
+    <?php
+
+      print render($content);
+    ?>
+
+    <div class="links-wrapper">
+      <?php print render($content['links']); ?>
+    </div>
+  </div>
+
+  <?php print render($content['comments']); ?>
+
+</div>
+<?php } ?>
